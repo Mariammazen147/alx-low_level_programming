70ec5cd3acc6eff20778e67144d02635a8fbf989
Mariam <marioumamazen@gmail.com>
2023-10-05 17:47:29 +0300

    5

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,15 @@
  *
  * Return: Always 0.
  */
-int main(void){
+int main(void)
+{
 	char x;
 
-	for (x = 'a'; x <= 'z'; x++){
+	for (x = 'a'; x <= 'z'; x++)
+	{
 		putchar(x);
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
