3a2bfe21aa94a905900c040432ae94632f66b468
Mariam <marioumamazen@gmail.com>
2023-10-12 20:22:08 +0300

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,13 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-*print_numbers - print 0123456789
-* 
-*Return: void
+* print_numbers - function that prints the numbers, from 0 to 9.
 */
-
 void print_numbers(void)
 {
 	char c;
 
 	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-		_putchar('\n');
+	_putchar(c);
+	_putchar('\n');
 }
